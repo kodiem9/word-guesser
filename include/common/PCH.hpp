1f8f5cf65dff3693682e214f8e3ee21ed6ba182e
kodiem9 <franczeskostr@gmail.com>
2025-01-19 19:15:09 +0100

    [wip] Program mode #1

--- include/common/PCH.hpp
+++ include/common/PCH.hpp
@@ -1,2 +1,6 @@
 #pragma once
 #include <iostream>
+#include <fstream>
+#include <sstream>
+#include <stdint.h>
+#include <unordered_map>
--- source/main.cpp
+++ source/main.cpp
@@ -1,6 +1,36 @@
 #include <PCH.hpp>
 
+constexpr uint16_t MODE_COUNT = 3;
+
+#define MODE_RANGE(mode) mode == 0 || mode > MODE_COUNT
+
+void SelectMode(uint16_t &mode) {
+    std::cout << "Select mode:" << std::endl;
+    std::cout << "1)\tTranslate left side to right side" << std::endl;
+    std::cout << "2)\tTranslate right side to left side" << std::endl;
+    std::cout << "3)\tQuit" << std::endl;
+    std::cin >> mode;
+
+    if (MODE_RANGE(mode)) {
+        std::cout << "Mode: " << mode << " does not exist. Please try again!" << std::endl;
+        SelectMode(mode);
+    }
+}
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::unordered_map<std::string, std::string> words;
+    std::string contents;
+    uint16_t mode;
+
+    // Reading file data
+    {
+        std::ifstream file("../data/words.txt");
+        std::stringstream buffer;
+        buffer << file.rdbuf();
+        contents = buffer.str();
+    }
+
+    SelectMode(mode);
+
     return 0;
 }
